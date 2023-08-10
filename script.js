eb5e3fa964f442335801ec512501813bfbe2e51c
Luiz <luizg.fd@hotmail.com>
2023-08-10 15:09:05 -0300

    atualizacao

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/assets/Avatar.png")
   }
 }
